2954fb66167e85982a90f28ad5c85d13b728b402
Pranjal Kumar <pranjalk449@gmail.com>
2024-06-11 16:49:06 +0530

    dark light theme add

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from "react";
-import "./App.css";
+import { useState } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import About from "./components/About";
 import Footer from "./components/Footer";
 import ContactUs from "./components/ContactUs";
+import "./App.css";
+
 function App() {
+  const [darkTheme, setDarkTheme] = useState(false);
+
+  const toggleTheme = () => {
+    setDarkTheme(!darkTheme);
+  };
+
   return (
-    <div className="App">
-      {/* Header Section */}
-      <Header />
+    <div className={darkTheme ? "App dark-theme" : "App light-theme"}>
+      <div className="scrolling-text">
+        <div>Welcome to Cake Creations!</div>
+      </div>
+      {/* Header Content Section */}
+      <Header toggleTheme={toggleTheme} darkTheme={darkTheme} />
       {/* Main Content Section */}
       <Main />
       {/* About Us Section */}
