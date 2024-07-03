73362253d3d60fe27a25f51cedba0194aa42caaf
Pranjal Kumar <pranjalk449@gmail.com>
2024-07-03 10:45:29 +0530

    .

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,22 +1,111 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ContactUs() {
+  const [formData, setFormData] = useState({
+    name: "",
+    contactNumber: "",
+    email: "",
+    message: "",
+  });
+
+  const [submitStatus, setSubmitStatus] = useState(null);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    if (name === "contactNumber") {
+      const contactNumber = value.replace(/[^0-9]/g, "").slice(0, 10);
+      setFormData({ ...formData, [name]: contactNumber });
+    } else {
+      setFormData({ ...formData, [name]: value });
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("https://getform.io/f/nadojevb", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      if (response.ok) {
+        setSubmitStatus("Success: Your message has been sent!");
+        setFormData({
+          name: "",
+          contactNumber: "",
+          email: "",
+          message: "",
+        });
+      } else {
+        setSubmitStatus("Error: Unable to send your message.");
+      }
+    } catch (error) {
+      setSubmitStatus("Error: Unable to send your message.");
+    }
+
+    // Clear the submission status after 3 seconds
+    setTimeout(() => {
+      setSubmitStatus(null);
+    }, 3000);
+  };
+
   return (
-    <div>
+    <main>
       <section id="contact" className="details">
         <h2>Contact Us</h2>
-        {/* Contact Form */}
-        <form action="mailto:pranjalkee010@gmail.com" method="post">
-          <input type="text" placeholder="Your Name" required />
-          <input type="number" placeholder="Contact Number" required />
-          <input type="email" placeholder="Your Email" required />
-          <textarea placeholder="Your Message"></textarea>
-          <button type="submit" class="stylish-button">
+        <form onSubmit={handleSubmit}>
+          <label>
+            <h4>Your Name</h4>
+            <input
+              type="text"
+              name="name"
+              placeholder="eg.Pranjal Kumar"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <label>
+            <h4>Contact Number</h4>
+            <input
+              type="text"
+              name="contactNumber"
+              placeholder="eg.1234567890"
+              value={formData.contactNumber}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <label>
+            <h4>Your Email</h4>
+            <input
+              type="email"
+              name="email"
+              placeholder="eg.pranjalkee010@gmail.com"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <label>
+            <h4>Your Message</h4>
+            <textarea
+              name="message"
+              placeholder="eg.Hi,I am Pranjal"
+              value={formData.message}
+              onChange={handleChange}
+            />
+          </label>
+          <button type="submit" className="stylish-button">
             Send Message
           </button>
         </form>
+        {submitStatus && <p>{submitStatus}</p>}
       </section>
-    </div>
+    </main>
   );
 }
+
 export default ContactUs;
