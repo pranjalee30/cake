d51b92cb994f81d53e7905befad23af657066d55
Pranjal Kumar <pranjalk449@gmail.com>
2024-08-14 09:59:09 +0530

    Added resposive css for all devices

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,8 @@
 import React from "react";
 import "./Header.css";
 import Switch from "react-switch";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faOpencart} from '@fortawesome/free-brands-svg-icons';
 
 function Header({ theme, toggleTheme }) {
 
@@ -15,7 +17,7 @@ function Header({ theme, toggleTheme }) {
         </a>
         <ul className="list">
           <li><a href="#home">Home</a></li>
-          <li><a href="#cart"> Add to Cart</a></li>
+          <li><a href="#cart"> <FontAwesomeIcon icon={faOpencart} />Cart</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#contact">Contact</a></li>
           <li><a href="#login">Login</a></li>
